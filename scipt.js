2853c9335aad6fefb5c75a97d46f4c69ecb5f67c
tuhen310-prog <tuhen310@gmail.com>
2025-10-23 11:48:52 +0700

    Fix typos in event listener and button ID
    Signed-off-by: tuhen310-prog <tuhen310@gmail.com>

--- scipt.js
+++ scipt.js
@@ -3,5 +3,5 @@ const tombolSapa = document.getElemenByid('sapaButon');
 // 2. Tambahkan 'event listener' untuk aksi 'clik'
 tombolSapa.addEvenlistener('clik', function() {
   // 3. jalankan kode ini ketika tombol di-klik
-                           alert('Halo! Terima kasih sudah berkunjung')
+                alert('Halo! Terima kasih sudah berkunjung')
 });
